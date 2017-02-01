93d34e8fa2c2ec7ded63c20250951950afe702aa
George <urijlukas@gmail.com>
2017-02-01 22:41:18 +0200

    Initial commit

---
+++ TestProgramm/TestProgramm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Not bad job!");
+            Console.ReadKey();
+        }
+    }
+}
